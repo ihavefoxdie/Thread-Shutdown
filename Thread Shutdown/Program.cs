d031708c56bd9e9fb27c1be2d63076641c2c0d3a
purpeliz <vladislav.nekrasov123@gmail.com>
2022-12-15 21:05:10 +0300

    Add project files.

---
+++ Thread Shutdown/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Testing
+{
+    public class Program
+    {
+        private static ManualResetEvent event1 = new(false);
+
+        public static void Waiting()
+        {
+            event1.WaitOne();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} is shutting down...");
+            Thread.Sleep(new Random().Next(1000, 10000));
+        }
+
+        public static void Main()
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                Thread thread = new(Waiting)
+                {
+                    Name = "Thread " + i.ToString(),
+                };
+                thread.Start();
+            }
+
+            Console.ReadLine();
+            event1.Set();
+        }
+    }
+}
